c4ce1307bd0e3659224cea2dd4bd7693306d1a30
galinov <galinov@umisoft.ru>
2017-03-07 19:16:50 +0300

    adaptive

---
+++ js/script.js
@@ -0,0 +1,35 @@
+(function( $ ) {
+
+	$(document).ready(function() {
+		var toggle = $('#navToggle'),
+			nav = $('#mobileNav');
+		initMobileNav(toggle, nav);
+		
+		
+		var selectToggle = $('#selectCategory');
+		
+		selectToggle.on('click', function() {
+			selectToggle.find('.select_content').slideToggle();
+		});
+
+		/**
+		 *
+		 * @param toggle
+		 * @param nav
+		 */
+		function initMobileNav(toggle, nav) {
+			toggle.on('click', function() {
+				nav.slideToggle();
+				makeCloseToggle($(this));
+			});
+		}
+
+		function makeCloseToggle(toggle) {
+			if (toggle.hasClass('close')) {
+				toggle.removeClass('close');
+				return true;
+			}
+			toggle.addClass('close');
+		}
+	});
+})(jQuery);
